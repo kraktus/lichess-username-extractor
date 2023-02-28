ad7152ddf58a16bc00ed5b705d9d445f5d1d869b
kraktus <kraktus@users.noreply.github.com>
2023-02-28 11:28:00 +0100

    Save results and add a progessbar

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,50 @@
 
 use std::{env, fs::File, io};
 
+use indicatif::{ProgressBar, ProgressStyle};
 use pgn_reader::BufferedReader;
 
 mod visitor;
 
-fn main() {
-    for arg in env::args().skip(1) {
-        let file = File::open(&arg).expect("fopen");
-
-        let uncompressed: Box<dyn io::Read> = if arg.ends_with(".zst") {
-            Box::new(zstd::Decoder::new(file).expect("zst decoder"))
-        } else if arg.ends_with(".bz2") {
-            Box::new(bzip2::read::MultiBzDecoder::new(file))
-        } else if arg.ends_with(".xz") {
-            Box::new(xz2::read::XzDecoder::new(file))
-        } else if arg.ends_with(".gz") {
-            Box::new(flate2::read::GzDecoder::new(file))
-        } else if arg.ends_with(".lz4") {
-            Box::new(lz4::Decoder::new(file).expect("lz4 decoder"))
-        } else {
-            Box::new(file)
-        };
-        let mut reader = BufferedReader::new(uncompressed);
-        let mut visitor = visitor::Usernames::default();
-        reader.read_all(&mut visitor).expect("Valid pgn file");
+pub fn get_progress_bar(nb_games: u64) -> ProgressBar {
+        let pb = ProgressBar::new(nb_games);
+        pb.set_style(
+            ProgressStyle::with_template(
+                "{msg} {spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {pos}/{len} ({eta})",
+            )
+            .expect("Invalid indicatif template syntax")
+            .progress_chars("#>-"),
+        );
+        pb
     }
+
+fn main() {
+    let path = env::args().nth(1).expect("pgn path expected");
+    let nb_games = env::args()
+        .next()
+        .and_then(|s| u64::from_str_radix(&s, 10).ok())
+        .expect("number of games of the pgn");
+    let file = File::open(&path).expect("fopen");
+
+    let uncompressed: Box<dyn io::Read> = if path.ends_with(".zst") {
+        Box::new(zstd::Decoder::new(file).expect("zst decoder"))
+    } else if path.ends_with(".bz2") {
+        Box::new(bzip2::read::MultiBzDecoder::new(file))
+    } else if path.ends_with(".xz") {
+        Box::new(xz2::read::XzDecoder::new(file))
+    } else if path.ends_with(".gz") {
+        Box::new(flate2::read::GzDecoder::new(file))
+    } else if path.ends_with(".lz4") {
+        Box::new(lz4::Decoder::new(file).expect("lz4 decoder"))
+    } else {
+        Box::new(file)
+    };
+    let mut reader = BufferedReader::new(uncompressed);
+
+
+    let mut visitor = visitor::Usernames::new(get_progress_bar(nb_games));
+    reader.read_all(&mut visitor).expect("Valid pgn file");
+    let mut usernames_vec: Vec<String> = visitor.usernames.into_iter().collect();
+    usernames_vec.sort();
+    std::fs::write("usernames.txt", usernames_vec.join("\n")).expect("write failed");
 }
--- src/visitor.rs
+++ src/visitor.rs
@@ -1,14 +1,29 @@
+use indicatif::ProgressBar;
 use pgn_reader::{RawHeader, Skip, Visitor};
 use rustc_hash::FxHashSet;
 
-#[derive(Default)]
 pub struct Usernames {
-    pub nb_game: usize,
+    pub games: usize,
     pub usernames: FxHashSet<String>,
+    pb: ProgressBar,
+}
+
+impl Usernames {
+    pub fn new(pb: ProgressBar) -> Self {
+        Self {
+            pb,
+            games: 0,
+            usernames: FxHashSet::default()
+        }
+    }
 }
 
 impl Visitor for Usernames {
     type Result = ();
+
+    fn begin_game(&mut self) {
+        self.pb.inc(1)
+    }
 
     fn header(&mut self, key: &[u8], value: RawHeader<'_>) {
         // Support games from a non-standard starting position.
@@ -19,7 +34,7 @@ impl Visitor for Usernames {
                     .unwrap_or_else(|e| {
                         panic!(
                             "{}",
-                            format!("Error {e} decoding username at game: {}", self.nb_game)
+                            format!("Error {e} decoding username at game: {}", self.games)
                         )
                     })
                     .to_string(),
